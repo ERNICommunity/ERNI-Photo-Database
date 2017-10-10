aa9a971882f2a67822db505f0882504e26aef674
Marek Linka <marek.linka@outlook.com>
2017-10-10 17:21:49 +0200

    Add UI prototype

---
+++ server/ERNI.PhotoDatabase.Server/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERNI.PhotoDatabase.Server.Controllers
+{
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
