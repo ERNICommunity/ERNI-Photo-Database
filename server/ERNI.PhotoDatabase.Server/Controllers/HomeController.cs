1db398ac31eb2d5ed7244c79dc133384e2fa79f5
Tomáš Kučečka <kuto@erni.sk>
2017-10-10 19:00:48 +0200

    Search functionality

--- server/ERNI.PhotoDatabase.Server/Controllers/HomeController.cs
+++ server/ERNI.PhotoDatabase.Server/Controllers/HomeController.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ERNI.PhotoDatabase.Server.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpGet]
-        public IActionResult Index()
+        private readonly DataProvider _dataProvider;
+
+        public HomeController(DataProvider dataProvider)
         {
-            return View();
+            _dataProvider = dataProvider;
+        }
+
+        [HttpGet]
+        public IActionResult Index(string query)
+        {
+            var images = _dataProvider.Images.Where(i => i.Tags.Contains(query))
+                .Select(i => new SearchResult {ImagePath = i.File})
+                .ToArray();
+
+            return View(images);
         }
     }
 }
