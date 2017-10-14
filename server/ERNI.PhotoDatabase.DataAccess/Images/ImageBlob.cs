ba5c7de30b872e4dd1ae2cd05a3566746dd67091
Rastislav Novotný <rasto@duracellko.net>
2017-10-14 16:50:12 +0200

    Add loading and saving images to and from Azure Blob Storage

---
+++ server/ERNI.PhotoDatabase.DataAccess/Images/ImageBlob.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERNI.PhotoDatabase.DataAccess.Images
+{
+    public class ImageBlob
+    {
+        public string Id { get; set; }
+
+        public byte[] Content { get; set; }
+    }
+}
