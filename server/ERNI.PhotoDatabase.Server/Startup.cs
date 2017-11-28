3e07e2e3c01ae08ae386f6bb5948ee35d34184e2
Andrej Mandli <info@mandli.sk>
2017-11-28 18:50:20 +0100

    do not delete db on startup

--- server/ERNI.PhotoDatabase.Server/Startup.cs
+++ server/ERNI.PhotoDatabase.Server/Startup.cs
@@ -49,10 +49,6 @@ namespace ERNI.PhotoDatabase.Server
             using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
             {
                 var context = serviceScope.ServiceProvider.GetRequiredService<DatabaseContext>();
-                if (env.IsDevelopment())
-                {
-                    context.Database.EnsureDeleted();
-                }
 
                 context.Database.EnsureCreated();
             }
